6041527a32df2a5dc005a6d9e2190124c681d380
paulogrillo <paulodigitalart@gmail.com>
2021-12-03 10:47:51 -0300

    Objetos

---
+++ objetos/aula01.js
@@ -0,0 +1,14 @@
+/* Criar objetos em Javascript */
+
+/* Primeira */
+var obj = new Object();
+/* Segunda */
+var obj = {};
+/* Terceira */
+obj = {
+  name: {
+    first: "Paulo",
+    last: "Grillo",
+  },
+  city: "São Paulo",
+};
---
+++ objetos/aula02.js
@@ -0,0 +1,20 @@
+/* Classe construtor */
+function Book(title, pages, cod) {
+  this.title = title;
+  this.pages = pages;
+  this.cod = cod;
+}
+
+/* Instânciar a classe */
+let book = new Book("Homo Sapiens", "50", "55553333222");
+
+/* Debug */
+// console.log("Título do livro:", book.title);
+// console.log("Páginas:", book.pages);
+// console.log("Código:", book.cod);
+
+/* Usando prototype para acessar uma cópia do atributo da classe nem ter que percorrer toda classe */
+Book.prototype.printTitle = function () {
+  console.log(this.title);
+};
+book.printTitle();
